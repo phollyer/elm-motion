904b3f54ab8837ffe59dbbfff373a0eee4843b75
Paul Hollyer <paul@hollyer.me.uk>
2026-08-27 14:22:34 +1000

    Tests(CssUnit): Add tests for the Transition engine, along with more scenarios, ready for API refactor

--- tests/Anim/Property/CssUnitSpec.elm
+++ tests/Anim/Property/CssUnitSpec.elm
@@ -14,6 +14,7 @@ style output via the WAAPI engine's `attributes` function.
 
 -}
 
+import Anim.Engine.Transition as Transition
 import Anim.Engine.WAAPI as WAAPI
 import Anim.Property.PerspectiveOrigin as PerspectiveOrigin
 import Anim.Property.Size as Size
@@ -38,14 +39,31 @@ fakeSubscriptionPort _ =
     Sub.none
 
 
-initWith : List (WAAPI.EngineBuilder -> WAAPI.EngineBuilder) -> WAAPI.AnimState msg
-initWith =
+initWAAPIWith : List (WAAPI.EngineBuilder -> WAAPI.EngineBuilder) -> WAAPI.AnimState msg
+initWAAPIWith =
     WAAPI.init fakeCommandPort fakeSubscriptionPort
 
 
-query : WAAPI.AnimState msg -> Query.Single msg
-query state =
+queryWAAPI : WAAPI.AnimState msg -> Query.Single msg
+queryWAAPI state =
     Html.div (WAAPI.attributes "el" state) []
+        |> Query.fromHtml
+
+
+initTransitionWith : List (Transition.EngineBuilder -> Transition.EngineBuilder) -> Transition.AnimState
+initTransitionWith =
+    Transition.init
+
+
+queryTransition : Transition.AnimState -> Query.Single msg
+queryTransition state =
+    Html.div (Transition.attributes "el" state) []
+        |> Query.fromHtml
+
+
+queryTransitionFor : String -> Transition.AnimState -> Query.Single msg
+queryTransitionFor animGroup state =
+    Html.div (Transition.attributes animGroup state) []
         |> Query.fromHtml
 
 
@@ -67,53 +85,210 @@ suite =
 translateTests : Test
 translateTests =
     describe "Translate.cssUnit"
-        [ test "default unit (Px) is used when cssUnit is not called" <|
+        [ describe "Transition Engine"
+            [ test "default unit (Px) is used when cssUnit is not called" <|
+                \_ ->
+                    initTransitionWith [ Translate.initXY "el" 50 25 ]
+                        |> queryTransition
+                        |> Query.has [ Selector.style "translate" "50px 25px 0px" ]
+            , test "cssUnit sets the unit for earlier initXY" <|
+                \_ ->
+                    initTransitionWith
+                        [ Translate.initXY "el" 50 25
+                            >> Translate.cssUnit Cqw
+                        ]
+                        |> queryTransition
+                        |> Query.has [ Selector.style "translate" "50cqw 25cqw 0px" ]
+            , test "cssUnitX overrides cssUnit on the X axis only" <|
+                \_ ->
+                    initTransitionWith
+                        [ Translate.initXY "el" 50 25
+                            >> Translate.cssUnit Cqw
+                            >> Translate.cssUnitX Vw
+                        ]
+                        |> queryTransition
+                        |> Query.has [ Selector.style "translate" "50vw 25cqw 0px" ]
+            , test "cssUnitY overrides cssUnit on the Y axis only" <|
+                \_ ->
+                    initTransitionWith
+                        [ Translate.initXY "el" 50 25
+                            >> Translate.cssUnit Cqw
+                            >> Translate.cssUnitY Vh
+                        ]
+                        |> queryTransition
+                        |> Query.has [ Selector.style "translate" "50cqw 25vh 0px" ]
+            , test "cssUnitZ sets the Z-axis unit" <|
+                \_ ->
+                    initTransitionWith
+                        [ Translate.initXYZ "el" 0 0 10
+                            >> Translate.cssUnitZ Vw
+                        ]
+                        |> queryTransition
+                        |> Query.has [ Selector.style "translate" "0px 0px 10vw" ]
+            , test "order matters - cssUnit before init has no effect" <|
+                \_ ->
+                    initTransitionWith
+                        [ Translate.cssUnit Cqw
+                            >> Translate.initXY "el" 50 25
+                        ]
+                        |> queryTransition
+                        |> Query.has [ Selector.style "translate" "50px 25px 0px" ]
+            , test "cssUnit is preserved during animation" <|
+                \_ ->
+                    let
+                        state =
+                            initTransitionWith
+                                [ Translate.initXY "el" 50 25
+                                    >> Translate.cssUnit Cqw
+                                ]
+                    in
+                    (Transition.animate state <|
+                        Transition.for "el"
+                            >> Translate.begin
+                            >> Translate.toXY 100 50
+                            >> Translate.end
+                    )
+                        |> queryTransition
+                        |> Query.has [ Selector.style "translate" "100cqw 50cqw 0px" ]
+            , test "cssUnit is changed for an animation" <|
+                \_ ->
+                    let
+                        state =
+                            initTransitionWith
+                                [ Translate.initXY "el" 50 25
+                                    >> Translate.cssUnit Cqw
+                                ]
+                    in
+                    (Transition.animate state <|
+                        Transition.for "el"
+                            >> Translate.cssUnit Cqh
+                            >> Translate.begin
+                            >> Translate.toXY 100 50
+                            >> Translate.end
+                    )
+                        |> queryTransition
+                        |> Query.has [ Selector.style "translate" "100cqh 50cqh 0px" ]
+            ]
+        , test "cssUnit is changed for only the targeted animation" <|
             \_ ->
-                initWith [ Translate.initXY "el" 50 25 ]
-                    |> query
-                    |> Query.has [ Selector.style "transform" "translate3d(50px, 25px, 0px)" ]
-        , test "cssUnit sets the unit for earlier initXY" <|
-            \_ ->
-                initWith
-                    [ Translate.initXY "el" 50 25
-                        >> Translate.cssUnit Cqw
+                let
+                    state =
+                        initTransitionWith
+                            [ Translate.initXY "el" 50 25
+                                >> Translate.cssUnit Cqw
+                            , Translate.initXY "el2" 50 25
+                                >> Translate.cssUnit Cqw
+                            ]
+
+                    animState =
+                        Transition.animate state <|
+                            Translate.cssUnit Cqh
+                                >> Transition.for "el"
+                                -- testing that this CSS Unit change does not affect the other animation
+                                -->> Translate.cssUnit Cqh
+                                >> Translate.begin
+                                >> Translate.toXY 100 50
+                                >> Translate.end
+                                >> Transition.for "el2"
+                                >> Translate.begin
+                                >> Translate.toXY 100 50
+                                >> Translate.end
+                in
+                Expect.all
+                    [ \_ ->
+                        animState
+                            |> queryTransitionFor "el"
+                            |> Query.has [ Selector.style "translate" "100cqh 50cqh 0px" ]
+                    , \_ ->
+                        animState
+                            |> queryTransitionFor "el2"
+                            |> Query.has [ Selector.style "translate" "100cqw 50cqw 0px" ]
                     ]
-                    |> query
-                    |> Query.has [ Selector.style "transform" "translate3d(50cqw, 25cqw, 0px)" ]
-        , test "cssUnitX overrides cssUnit on the X axis only" <|
+                    ()
+        , test "cssUnit is changed for all animations" <|
             \_ ->
-                initWith
-                    [ Translate.initXY "el" 50 25
-                        >> Translate.cssUnit Cqw
-                        >> Translate.cssUnitX Vw
+                let
+                    state =
+                        initTransitionWith
+                            [ Translate.initXY "el" 50 25
+                                >> Translate.cssUnit Cqw
+                            , Translate.initXY "el2" 50 25
+                                >> Translate.cssUnit Cqw
+                            ]
+
+                    animState =
+                        Transition.animate state <|
+                            -- testing that this CSS Unit change does change for all animations
+                            Transition.cssUnit Cqh
+                                >> Transition.for "el"
+                                >> Translate.begin
+                                >> Translate.toXY 100 50
+                                >> Translate.end
+                                >> Transition.for "el2"
+                                >> Translate.begin
+                                >> Translate.toXY 100 50
+                                >> Translate.end
+                in
+                Expect.all
+                    [ \_ ->
+                        animState
+                            |> queryTransitionFor "el"
+                            |> Query.has [ Selector.style "translate" "100cqh 50cqh 0cqh" ]
+                    , \_ ->
+                        animState
+                            |> queryTransitionFor "el2"
+                            |> Query.has [ Selector.style "translate" "100cqh 50cqh 0cqh" ]
                     ]
-                    |> query
-                    |> Query.has [ Selector.style "transform" "translate3d(50vw, 25cqw, 0px)" ]
-        , test "cssUnitY overrides cssUnit on the Y axis only" <|
-            \_ ->
-                initWith
-                    [ Translate.initXY "el" 50 25
-                        >> Translate.cssUnit Cqw
-                        >> Translate.cssUnitY Vh
-                    ]
-                    |> query
-                    |> Query.has [ Selector.style "transform" "translate3d(50cqw, 25vh, 0px)" ]
-        , test "cssUnitZ sets the Z-axis unit" <|
-            \_ ->
-                initWith
-                    [ Translate.initXYZ "el" 0 0 10
-                        >> Translate.cssUnitZ Vw
-                    ]
-                    |> query
-                    |> Query.has [ Selector.style "transform" "translate3d(0px, 0px, 10vw)" ]
-        , test "order matters - cssUnit before init has no effect" <|
-            \_ ->
-                initWith
-                    [ Translate.cssUnit Cqw
-                        >> Translate.initXY "el" 50 25
-                    ]
-                    |> query
-                    |> Query.has [ Selector.style "transform" "translate3d(50px, 25px, 0px)" ]
+                    ()
+        , describe "WAAPI Engine"
+            [ test "default unit (Px) is used when cssUnit is not called" <|
+                \_ ->
+                    initWAAPIWith [ Translate.initXY "el" 50 25 ]
+                        |> queryWAAPI
+                        |> Query.has [ Selector.style "transform" "translate3d(50px, 25px, 0px)" ]
+            , test "cssUnit sets the unit for earlier initXY" <|
+                \_ ->
+                    initWAAPIWith
+                        [ Translate.initXY "el" 50 25
+                            >> Translate.cssUnit Cqw
+                        ]
+                        |> queryWAAPI
+                        |> Query.has [ Selector.style "transform" "translate3d(50cqw, 25cqw, 0px)" ]
+            , test "cssUnitX overrides cssUnit on the X axis only" <|
+                \_ ->
+                    initWAAPIWith
+                        [ Translate.initXY "el" 50 25
+                            >> Translate.cssUnit Cqw
+                            >> Translate.cssUnitX Vw
+                        ]
+                        |> queryWAAPI
+                        |> Query.has [ Selector.style "transform" "translate3d(50vw, 25cqw, 0px)" ]
+            , test "cssUnitY overrides cssUnit on the Y axis only" <|
+                \_ ->
+                    initWAAPIWith
+                        [ Translate.initXY "el" 50 25
+                            >> Translate.cssUnit Cqw
+                            >> Translate.cssUnitY Vh
+                        ]
+                        |> queryWAAPI
+                        |> Query.has [ Selector.style "transform" "translate3d(50cqw, 25vh, 0px)" ]
+            , test "cssUnitZ sets the Z-axis unit" <|
+                \_ ->
+                    initWAAPIWith
+                        [ Translate.initXYZ "el" 0 0 10
+                            >> Translate.cssUnitZ Vw
+                        ]
+                        |> queryWAAPI
+                        |> Query.has [ Selector.style "transform" "translate3d(0px, 0px, 10vw)" ]
+            , test "order matters - cssUnit before init has no effect" <|
+                \_ ->
+                    initWAAPIWith
+                        [ Translate.cssUnit Cqw
+                            >> Translate.initXY "el" 50 25
+                        ]
+                        |> queryWAAPI
+                        |> Query.has [ Selector.style "transform" "translate3d(50px, 25px, 0px)" ]
+            ]
         ]
 
 
@@ -130,7 +305,7 @@ sizeTests =
             \_ ->
                 let
                     rendered =
-                        initWith [ Size.initHW "el" 80 120 ] |> query
+                        initWAAPIWith [ Size.initHW "el" 80 120 ] |> queryWAAPI
                 in
                 Expect.all
                     [ \_ -> rendered |> Query.has [ Selector.style "height" "80px" ]
@@ -141,11 +316,11 @@ sizeTests =
             \_ ->
                 let
                     rendered =
-                        initWith
+                        initWAAPIWith
                             [ Size.initHW "el" 80 120
                                 >> Size.cssUnit Cqmin
                             ]
-                            |> query
+                            |> queryWAAPI
                 in
                 Expect.all
                     [ \_ -> rendered |> Query.has [ Selector.style "height" "80cqmin" ]
@@ -156,12 +331,12 @@ sizeTests =
             \_ ->
                 let
                     rendered =
-                        initWith
+                        initWAAPIWith
                             [ Size.initHW "el" 80 120
                                 >> Size.cssUnit Cqmin
                                 >> Size.cssUnitW Vw
                             ]
-                            |> query
+                            |> queryWAAPI
                 in
                 Expect.all
                     [ \_ -> rendered |> Query.has [ Selector.style "height" "80cqmin" ]
@@ -172,12 +347,12 @@ sizeTests =
             \_ ->
                 let
                     rendered =
-                        initWith
+                        initWAAPIWith
                             [ Size.initHW "el" 80 120
                                 >> Size.cssUnit Cqmin
                                 >> Size.cssUnitH Vh
                             ]
-                            |> query
+                            |> queryWAAPI
                 in
                 Expect.all
                     [ \_ -> rendered |> Query.has [ Selector.style "height" "80vh" ]
@@ -198,49 +373,49 @@ perspectiveOriginTests =
     describe "PerspectiveOrigin.cssUnit"
         [ test "default unit (Percent) is used when cssUnit is not called" <|
             \_ ->
-                initWith [ PerspectiveOrigin.initXY "el" 50 75 ]
-                    |> query
+                initWAAPIWith [ PerspectiveOrigin.initXY "el" 50 75 ]
+                    |> queryWAAPI
                     |> Query.has [ Selector.style "perspective-origin" "50% 75%" ]
         , test "cssUnit switches the unit for earlier initXY" <|
             \_ ->
-                initWith
+                initWAAPIWith
                     [ PerspectiveOrigin.initXY "el" 200 150
                         >> PerspectiveOrigin.cssUnit Px
                     ]
-                    |> query
+                    |> queryWAAPI
                     |> Query.has [ Selector.style "perspective-origin" "200px 150px" ]
         , test "cssUnitX overrides cssUnit on the X axis only" <|
             \_ ->
-                initWith
+                initWAAPIWith
                     [ PerspectiveOrigin.initXY "el" 50 150
                         >> PerspectiveOrigin.cssUnit Px
                         >> PerspectiveOrigin.cssUnitX Percent
                     ]
-                    |> query
+                    |> queryWAAPI
                     |> Query.has [ Selector.style "perspective-origin" "50% 150px" ]
         , test "cssUnitY overrides cssUnit on the Y axis only" <|
             \_ ->
-                initWith
+                initWAAPIWith
                     [ PerspectiveOrigin.initXY "el" 200 50
                         >> PerspectiveOrigin.cssUnit Px
                         >> PerspectiveOrigin.cssUnitY Percent
                     ]
-                    |> query
+                    |> queryWAAPI
                     |> Query.has [ Selector.style "perspective-origin" "200px 50%" ]
         , test "initX uses the unit selected by cssUnitX" <|
             \_ ->
-                initWith
+                initWAAPIWith
                     [ PerspectiveOrigin.initX "el" 200
                         >> PerspectiveOrigin.cssUnitX Px
                     ]
-                    |> query
+                    |> queryWAAPI
                     |> Query.has [ Selector.style "perspective-origin" "200px 50%" ]
         , test "initY uses the unit selected by cssUnitY" <|
             \_ ->
-                initWith
+                initWAAPIWith
                     [ PerspectiveOrigin.initY "el" 150
                         >> PerspectiveOrigin.cssUnitY Px
                     ]
-                    |> query
+                    |> queryWAAPI
                     |> Query.has [ Selector.style "perspective-origin" "50% 150px" ]
         ]
